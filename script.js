b68b8ad8124f379583a7dd19d8497cecd225da28
Oliko136 <olchick136@gmail.com>
2025-09-02 21:22:21 +0400

    Add edit mode feature

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 const clearBtn = document.getElementById('clear');
 const itemFilter = document.getElementById('filter');
+const formBtn = itemForm.querySelector('button');
+let isEditMode = false;
 
 // Display items to UI from local storage
 function displayItems() {
@@ -23,6 +25,16 @@ function onAddItemSubmit(e) {
     if (newItem === '') {
         alert('Please add an item');
         return;
+    }
+
+    // Check for edit mode
+    if (isEditMode) {
+        const itemToEdit = itemList.querySelector('.edit-mode');
+
+        removeItemFromStorage(itemToEdit.textContent);
+        itemToEdit.classList.remove('edit-mode');
+        itemToEdit.remove();
+        isEditMode = false;
     }
 
     // Create item DOM element
@@ -89,6 +101,10 @@ function onClickItem(e) {
     if (e.target.parentElement.classList.contains('remove-item')) {
         removeItem(e.target.parentElement.parentElement);
     }
+
+    if (e.target.tagName === 'LI') {
+        setItemToEdit(e.target);
+    }
 }
 
 // Remove item feature
@@ -112,6 +128,18 @@ function removeItemFromStorage(item) {
 
     // Re-set to local storage
     localStorage.setItem('items', JSON.stringify(itemsFromStorage));
+}
+
+// Set item to edit mode feature
+function setItemToEdit(item) {
+    isEditMode = true;
+
+    itemList.querySelectorAll('li').forEach(i => i.classList.remove('edit-mode'));
+
+    item.classList.add('edit-mode');
+    formBtn.innerHTML = '<i class="fa-solid fa-pen"></i> Update Item';
+    formBtn.style.backgroundColor = '#228B22';
+    itemInput.value = item.textContent;
 }
 
 // Clear all items feature
@@ -142,6 +170,8 @@ function filterItems(e) {
 
 // Check and clear UI state feature
 function checkUI() {
+    itemInput.value = '';
+
     const items = itemList.querySelectorAll('li');
 
     if (items.length === 0) {
@@ -151,6 +181,11 @@ function checkUI() {
         clearBtn.style.display = 'block';
         itemFilter.style.display = 'block';
     }
+    
+    formBtn.innerHTML = '<i class="fa-solid fa-plus"></i> Add Item';
+    formBtn.style.backgroundColor = '#333';
+
+    isEditMode = false;
 }
 
 // Initialize app
